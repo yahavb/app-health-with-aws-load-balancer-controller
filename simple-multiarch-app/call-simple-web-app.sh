ada378cb527c8bf5b7e673834014f71ad5e31717
yahavb <yahavb@amazon.com>
2023-10-12 10:44:43 -0700

    remove cw pub

--- simple-multiarch-app/call-simple-web-app.sh
+++ simple-multiarch-app/call-simple-web-app.sh
@@ -3,6 +3,6 @@ while true
 do
   cmd="curl -o /dev/null -sL ${APP_URL}/runtime/" 
   http_response_time=`TIMEFORMAT="%R"; { time $cmd; } 2>&1`
-  aws cloudwatch put-metric-data --metric-name http_response_time --namespace ${DEPLOY_NAME} --value $http_response_time
+  #aws cloudwatch put-metric-data --metric-name http_response_time --namespace ${DEPLOY_NAME} --value $http_response_time
   sleep $SLEEP_BETWEEN_CYCLE
 done
