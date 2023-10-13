51c27648a7e911052c5b10341591cce52afe3c87
yahavb <yahavb@amazon.com>
2023-10-13 12:48:08 -0700

    pub instance arc to cw

--- simple-multiarch-app/start.sh
+++ simple-multiarch-app/start.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
 export ARCH=$(uname -m)
 aws cloudwatch put-metric-data --metric-name node_instance_arch --namespace ${DEPLOY_NAME} --value $ARCH
+echo $?
 python ./manage.py runserver 0.0.0.0:8000
